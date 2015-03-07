325d6e5099a70dcc9ffa4d9d817218e2383f9071
Kenneth Feng <kennethzfeng@gmail.com>
2015-03-07 16:53:40 -0500

    Update for Flask

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 import os
-from flask import Flask
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'Hello, World'
+from app import app
 
 
 if __name__ == '__main__':
     # Read port selected by the cloud for our application
     PORT = int(os.getenv('VCAP_APP_PORT', 8000))
-    app.run(host='0.0.0.0', port=PORT)
+
+    ENV = os.getenv('ENV')
+    if ENV is not None and ENV == "DEV":
+        debug = True
+    else:
+        debug = False
+
+    app.run(host='0.0.0.0', port=PORT, debug=debug)
