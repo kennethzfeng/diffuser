42bb81aec046117e84e3f55e10ca01023884a0ca
Kenneth Feng <kennethzfeng@gmail.com>
2015-03-08 14:07:19 -0400

    Move the Poll view to Angular

--- app/static/app.js
+++ app/static/app.js
@@ -2,3 +2,13 @@ var hello = "Hello";
 
 console.log(hello);
 
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+]).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
