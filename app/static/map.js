d4c623fb45d94abde7ab45fcaa323d1b3cbf1e51
Kenneth Feng <kennethzfeng@gmail.com>
2015-03-08 10:25:01 -0400

    Refactor map.js

--- app/static/map.js
+++ app/static/map.js
@@ -2,17 +2,57 @@ var directionsDisplay;
 var map;
 
 var locations = [
-  ['Starbucks 684 Avenue of the Americas', 40.741843, -73.993232, 4],
-  ['Starbucks 41 Union Square W', 40.7371162, -73.9906153, 5],
-  ['Starbucks 304 Park Ave S', 40.7400682, -73.9868082, 3],
-  ['Starbucks 10 Union Square E', 40.7348529, -73.9897794, 2],
-  ['Starbucks 510 Avenue of the Americas', 40.7368034, -73.9968884, 1]
+  { 
+    "id": 5,
+    "name": "Starbucks 684 Avenue of the Americas",
+    "coordinate": { "lat": 40.741843, "lng": -73.993232 },
+  },
+  { 
+    "id": 4,
+    "name": "Starbucks 41 Union Square W",
+    "coordinate": { "lat": 40.7371162, "lng": -73.9906153 }
+  },
+  { 
+    "id": 3,
+    "name": "Starbucks 304 Park Ave S",
+    "coordinate": { "lat": 40.7400682, "lng": -73.9868082 }
+  },
+  { 
+    "id": 2,
+    "name": "Starbucks 10 Union Square E",
+    "coordinate": { "lat": 40.7348529, "lng": -73.9897794 }
+  },
+  { 
+    "id": 1,
+    "name": "Starbucks 510 Avenue of the Americas",
+    "coordinate": { "lat": 40.7368034, "lng": -73.9968884 }
+  }
 ];
 
 
 var infowindow = new google.maps.InfoWindow();
 
 var marker, i;
+
+function plotLocations(locations, map) {
+  var location;
+  for (i = 0; i < locations.length; i++) {
+    location = locations[i];
+
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(location.coordinate.lat, location.coordinate.lng),
+      map: map
+    });
+
+    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+      return function () {
+        infowindow.setContent(location.name);
+        infowindow.open(map, marker);
+      };
+    })(marker, i));
+  }
+}
+
 function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 10,
@@ -20,19 +60,7 @@ function initialize() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
-  for (i = 0; i < locations.length; i++) {
-    marker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-      map: map
-    });
-
-    google.maps.event.addListener(marker, 'click', (function (marker, i) {
-      return function () {
-        infowindow.setContent(locations[i][0]);
-        infowindow.open(map, marker);
-      };
-    })(marker, i));
-  }
+  plotLocations(locations, map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
